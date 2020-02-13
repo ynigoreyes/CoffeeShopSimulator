ebb4229516c57932ab70845e6f431b9d621979b1
rajeev <rajeev.goonie98@gmail.com>
2020-02-12 22:00:51 -0800

    added abstracted validateEvent method from super

--- src/CoffeeShopSimulator/EventBus/Events/CustomerLeavesEvent.java
+++ src/CoffeeShopSimulator/EventBus/Events/CustomerLeavesEvent.java
@@ -16,4 +16,26 @@ public class CustomerLeavesEvent extends CoffeeShopEvent {
     public Customer getCustomer() {
         return customer;
     }
+
+    /**
+     * This is an abstracted method from the CoffeeShopEvent class.
+     * This is to validate the Event that occurs has a valid Customer
+     *      passed to this object.
+     *
+     * @param data Object to be validated
+     * @return If the data passed is not null and is an instance of Customer
+     */
+    public boolean validateEvent(Object data){
+        if(data == null){
+            System.out.println("Customer cannot be null!!!");
+            return false;
+        }
+
+        if(! (data instanceof Customer) ){
+            System.out.println("Data must be a Customer!!!");
+            return false;
+        }
+
+        return true;
+    }
 }
