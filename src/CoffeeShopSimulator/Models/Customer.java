324619f061b22ce7de83ecbbfb766ca3777bd9ab
rajeev <rajeev.goonie98@gmail.com>
2020-02-12 22:40:00 -0800

    override defaulted toString

--- src/CoffeeShopSimulator/Models/Customer.java
+++ src/CoffeeShopSimulator/Models/Customer.java
@@ -4,4 +4,8 @@ public class Customer extends Person {
     public Customer(String name) {
         super(name);
     }
+
+    public String toString(){
+        return super.toString() + "(a Customer)";
+    }
 }
