fe8d429a6ba9e93ce7d307bf657aa23de31f69f1
rajeev <rajeev.goonie98@gmail.com>
2020-02-14 14:27:15 -0800

    changed constructors

--- src/CoffeeShopSimulator/ShopExampleProcedurally/Shop.java
+++ src/CoffeeShopSimulator/ShopExampleProcedurally/Shop.java
@@ -1,12 +1,17 @@
 package CoffeeShopSimulator.ShopExampleProcedurally;
 
+import java.util.ArrayList;
+
 public class Shop{
 
     private Boss manager;
     private Employee barista;
-    private Guest customer;
+    private ArrayList<Guest> customer;
 
-    public Shop(Boss b, Employee e, Guest g){}
-
+    public Shop(Boss b, Employee e){
+        manager = b;
+        barista = e;
+        customer = new ArrayList<>();
+    }
 
 }
