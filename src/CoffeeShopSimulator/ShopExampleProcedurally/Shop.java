56ef78c43c16ef4fd54cc5a2bea6bbaa521202f9
rajeev <rajeev.goonie98@gmail.com>
2020-02-14 14:11:57 -0800

    created the shop

---
+++ src/CoffeeShopSimulator/ShopExampleProcedurally/Shop.java
@@ -0,0 +1,12 @@
+package CoffeeShopSimulator.ShopExampleProcedurally;
+
+public class Shop{
+
+    private Boss manager;
+    private Employee barista;
+    private Guest customer;
+
+    public Shop(Boss b, Employee e, Guest g){}
+
+
+}
