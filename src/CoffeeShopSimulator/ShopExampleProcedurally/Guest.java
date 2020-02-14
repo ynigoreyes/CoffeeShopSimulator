404c5a1ec4b45e91d61804c094ae69a841291fbd
EmilyQElizabeth <emilyequintanilla@gmail.com>
2020-02-14 17:12:47 -0600

    more changes

--- src/CoffeeShopSimulator/ShopExampleProcedurally/Guest.java
+++ src/CoffeeShopSimulator/ShopExampleProcedurally/Guest.java
@@ -1,25 +1,37 @@
 package CoffeeShopSimulator.ShopExampleProcedurally;
 
+import java.util.Scanner;
+
 public class Guest{
 
     private String name;
     String getReceipt, menuItem;
 
-
-    public void Guest(String name) {
+    public void Guest(String name, Scanner input) {
         this.name = name;
 
-        input.
-                System.out.println("My name for this order is" + name + ".");
-
+        input.nextLine();
+        //customer gives their name to the employee for the order
+        //customer name can be called when the order is made by employee
+        System.out.println("My name for this order is" + name + ".");
     }
 
-    public void RecieveReceipt(){
+    public void orderMenuItems(Scanner input){
 
-        input.
-        System.out.println("My name for this order is " + name + ".");
+        input.nextLine();
+        //customer chooses item(s) off the menu created by the boss
+        //menu rotates every certain # of drinks
         System.out.println("I would like " + menuItem + "please!");
+    }
+
+    public void payForOrder(){
+        
+    }
+
+    public void RecieveReceipt(Scanner input){
+
+        input.nextLine();
+        //customer receives receipt from the employee with payment total
         System.out.println(getReceipt + "Thank you!");
     }
-    
 }
