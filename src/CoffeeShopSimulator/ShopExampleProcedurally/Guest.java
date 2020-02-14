7f9e2ef28e27245f2e0bd92d1808918d78fa5384
EmilyQElizabeth <emilyequintanilla@gmail.com>
2020-02-14 16:48:00 -0600

    customer name

--- src/CoffeeShopSimulator/ShopExampleProcedurally/Guest.java
+++ src/CoffeeShopSimulator/ShopExampleProcedurally/Guest.java
@@ -3,9 +3,11 @@ package CoffeeShopSimulator.ShopExampleProcedurally;
 public class Guest{
 
     private String name;
+    String getName, getReceipt, orderProducts;
 
     public Guest(String name){
         this.name = name;
-    }
 
+        System.out.print("My name for this order is" + getName);
+    }
 }
