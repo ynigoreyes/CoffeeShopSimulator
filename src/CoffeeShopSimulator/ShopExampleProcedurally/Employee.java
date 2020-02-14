11f0ce7138c56e14b203c41b6e773ce78f99ad49
rajeev <rajeev.goonie98@gmail.com>
2020-02-14 15:40:24 -0800

    fixed errors

--- src/CoffeeShopSimulator/ShopExampleProcedurally/Employee.java
+++ src/CoffeeShopSimulator/ShopExampleProcedurally/Employee.java
@@ -1,23 +1,23 @@
+package CoffeeShopSimulator.ShopExampleProcedurally;
+
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.ArrayList;
 import java.util.List;
 
-package CoffeeShopSimulator.ShopExampleProcedurally;
 
 public class Employee{
 
     private String name;
     private Queue<Order> orders = new LinkedList<>();
-    private <String> orderPutOut = new ArrayList<>();
+    private ArrayList<String> orderPutOut = new ArrayList<>();
 
     public Employee(String name){
         this.name = name;
     }
 
-    // take coustomer order
+    // take customer order
     public void takeOrder(String name, String order){
-        //order is an array of size 2 the first element is the coustomer name the 2nd is their order
         Order newOrder = new Order(name, order);
         orders.add(newOrder);
     }
@@ -25,9 +25,9 @@ public class Employee{
     // make and call order
     public void makeNextOrder(){
         //add order to ___ for pickup
-        orderPutOut.add(orders.head);
-        //
-        System.out.println("I have an order for " + orders.head[0]);
+        orderPutOut.add( orders.peek().guestName );
+
+        System.out.println("I have an order for " + orders.peek() );
         //remove order from orders
         orders.remove();
     }
