735311ef8368ff6f637358456fd13fb76a29bb9d
ColeChip <ColemanChipman@gmail.com>
2020-02-14 16:47:12 -0600

    Added Pay Employee Method

--- src/CoffeeShopSimulator/ShopExampleProcedurally/Boss.java
+++ src/CoffeeShopSimulator/ShopExampleProcedurally/Boss.java
@@ -3,9 +3,20 @@ package CoffeeShopSimulator.ShopExampleProcedurally;
 public class Boss{
 
 	private String name;
+	private Hashtable Supplies = new HashTable();
+	private String[] Menu1 = {"Mocha", "Frappe", "Latte", "Coffee"};
+	private String[] Menu2 = {"Machiato", "Latte", "Coffee"};
+	private String[] Menu3 = {"Mocha", "Frappe", "Latte"};
 
-	public Boss(String name){
+	public Boss(String name)
+	{
 		this.name = name;
+	}
+
+	public double PayEmployee(int Orders) // Based on Orders Served Pays Employee 50c on the Order
+	{
+		double pay = Orders * .50;
+		return pay;
 	}
 
 }
