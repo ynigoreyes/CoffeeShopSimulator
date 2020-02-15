df7219cea3d62f34243ce4663011f898be3fac1f
rajeev <rajeev.goonie98@gmail.com>
2020-02-14 16:57:13 -0800

    realized no use for get receipt... sorry Emily...

--- src/CoffeeShopSimulator/ShopExampleProcedurally/Guest.java
+++ src/CoffeeShopSimulator/ShopExampleProcedurally/Guest.java
@@ -15,11 +15,6 @@ public class Guest{
         return menuItem;
     }
 
-    public void ReceiveReceipt(){
-        //customer receives receipt from the employee with payment total
-        System.out.println(getReceipt + "Thank you!");
-    }
-
     @Override
     public String toString(){
         return name;
