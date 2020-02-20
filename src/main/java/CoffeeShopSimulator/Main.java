d0d80752bcefd09faabc89e0c0b5244536613024
Miggy Reyes <ynigoreyes@gmail.com>
2020-02-20 11:06:35 -0600

    adding maven deps

--- src/main/java/CoffeeShopSimulator/Main.java
+++ src/main/java/CoffeeShopSimulator/Main.java
@@ -1,10 +1,6 @@
 package CoffeeShopSimulator;
 
 import CoffeeShopSimulator.Utilities.Logger;
-import ShopExampleProcedurally.Data.Boss;
-import ShopExampleProcedurally.Data.Employee;
-import ShopExampleProcedurally.Data.Guest;
-import ShopExampleProcedurally.Data.Shop;
 import com.google.common.eventbus.EventBus;
 
 public class Main {
