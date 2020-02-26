a9820f1d1037aa02f80fa898366b55a3251821d6
rajeev <rajeev.goonie98@gmail.com>
2020-02-26 09:20:28 -0800

    added comment to instance var

--- src/main/java/CoffeeShopSimulator/Models/Customer.java
+++ src/main/java/CoffeeShopSimulator/Models/Customer.java
@@ -2,7 +2,7 @@ package CoffeeShopSimulator.Models;
 
 public class Customer extends Person {
 
-    private CustomerStates currentState = CustomerStates.WALKED_IN;
+    private CustomerStates currentState = CustomerStates.WALKED_IN; //all customers start in initial state
 
     public Customer(String name) {
         super(name);
