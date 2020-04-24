5315c610b3aee080277885905dae98b82d59f1eb
rajeev <rajeev.goonie98@gmail.com>
2020-04-24 14:56:22 -0700

    removed states since they only do two things

--- src/main/java/CoffeeShopSimulator/Models/Manager.java
+++ src/main/java/CoffeeShopSimulator/Models/Manager.java
@@ -1,18 +1,11 @@
 package CoffeeShopSimulator.Models;
 
-import CoffeeShopSimulator.Models.States.ManagerStates;
-
 public class Manager extends Person {
 
-    private ManagerStates currentState = ManagerStates.READY;  //manager will start in initial state
 
     public Manager(String name) {
         super(name);
     }
-
-    public void setCurrentState(ManagerStates state){ this.currentState = state; }
-
-    public ManagerStates getCurrentState(){ return this.currentState; }
 
     @Override
     public String toString(){
